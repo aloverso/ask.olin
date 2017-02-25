53f725b91ce3fa1e14918d1d2227ec88e395e1b7
MatthewBeaudouinLafon <matthew.beaudouin-lafon@students.olin.edu>
2017-02-25 11:08:57 -0500

    Python3-ified

--- server.py
+++ server.py
@@ -30,10 +30,10 @@ users = db.users
 # From: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
 def levenshtein(seq1, seq2):
     oneago = None
-    thisrow = range(1, len(seq2) + 1) + [0]
-    for x in xrange(len(seq1)):
+    thisrow = list(range(1, len(seq2) + 1)) + [0]
+    for x in range(len(seq1)):
         twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
-        for y in xrange(len(seq2)):
+        for y in range(len(seq2)):
             delcost = oneago[y] + 1
             addcost = thisrow[y - 1] + 1
             subcost = oneago[y - 1] + (seq1[x] != seq2[y])
